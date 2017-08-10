c2b3a909c2d0595337a9bc6f00a4af098ec75800
DimaMukhin <dima9348@gmail.com>
2017-08-10 14:01:21 -0500

    books reducer

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: 'Head First Java', pages: 804 },
+    { title: '28 days', pages: 28 },
+    { title: 'Lord of the rings', pages: 1301 },
+    { title: 'A song of ice and fire', pages: 904 }
+  ];
+}
